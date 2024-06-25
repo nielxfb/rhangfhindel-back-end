201b99e6a9d169065bc707e14d23f39bd9c60862
Daniel Adamlu <adamludnl@gmail.com>
2024-06-26 00:44:55 +0700

    fix: port logging

--- index.js
+++ index.js
@@ -96,5 +96,5 @@ app.post("/api/send-notification", (req, res) => {
 const port = process.env.PORT || 3000;
 
 app.listen(port, "0.0.0.0", () => {
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on port ${port}`);
 });
