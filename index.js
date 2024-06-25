2f2acc736f28bd31b2c56120f841dcb703448e86
Daniel Adamlu <adamludnl@gmail.com>
2024-06-26 00:53:17 +0700

    fix: long private key env handling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -6,11 +6,12 @@ const admin = require("firebase-admin");
 const cors = require("cors");
 const fs = require("fs");
 
+const { privateKey } = process.env.FIREBASE_PRIVATE_KEY;
+
 const accountData = fs.readFileSync("rhang-fhindel-9cefdb21ebbb.json");
 const serviceAccount = JSON.parse(accountData);
-serviceAccount["private_key_id"] = process.env.FIREBASE_PRIVATE_KEY_ID
-serviceAccount["private_key"] = process.env.FIREBASE_PRIVATE_KEY
-
+serviceAccount["private_key_id"] = process.env.FIREBASE_PRIVATE_KEY_ID;
+serviceAccount["private_key"] = privateKey;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
